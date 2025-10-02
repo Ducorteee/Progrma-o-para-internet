8b61fe09024575972ab0c213cdaf8d00f083cde6
Ducorteee <k.cordeiro@escolar.ifrn.edu.br>
2025-10-02 16:15:03 -0300

    função criar e lista feito

--- todo-list/todo-list2.php
+++ todo-list/todo-list2.php
@@ -7,12 +7,37 @@ $localhost = 'localhost';
 $usuario = 'root';
 $senha = '';
 $database = 'todo_list';
-$conn =mysqli_connect($localhost,$usuario,$senha,$database);
+$conn =new mysqli($localhost,$usuario,$senha,$database);
 
  if($conn -> connect_error){
     die('Deu erro ao conectar'.mysqli_connect_error());
  }
-$tarefas = []
+
+
+ # criacao de tarefa
+
+ if(isset($_POST['descricao']) && !empty(trim($_POST['descricao']))){
+   $descricao = $conn -> real_escape_string($_POST['descricao']);
+   $sqlcreate =  "INSERT INTO tarefas (descricao) VALUES ('$descricao')";
+   
+   if($conn -> query($sqlcreate) === TRUE){
+      header("location: todo-list2.php");
+   }
+ }
+
+ # lista tarefas  
+ $tarefas = [];
+ $sqlselect = "SELECT * FROM tarefas ORDER BY data_criacao DESC";
+ $result = $conn -> query($sqlselect);
+
+ if($result -> num_rows >0){
+    while($row = $result -> fetch_assoc()){
+   $tarefas [] = $row;
+
+    } 
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -32,7 +57,13 @@ $tarefas = []
 
     <h2>Suas tarefas</h2>
     <?php if(!empty($tarefas)):?>
-    <h3>Suas tarefas</h3>
+    <ul>
+     <?php foreach($tarefas as $tarefas):?>
+        <li>
+        <?php echo $tarefas ['descricao']?>
+        </li>
+     <?php endforeach;?>
+    </ul>
     <?php else:?>
     <h3>Não tem tarefas</h3>
     <?php endif;?>
